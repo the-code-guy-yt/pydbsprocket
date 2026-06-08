38f71d74b488cbc5ccefd5ce2e8283e36f1a95e1
the_code_guy_yt <altek2k@gmail.com>
2026-06-08 18:04:38 +0100

    Renamed provider_mapper to provider discover to cover change in design Original design had providers hardcoded in the dict, while faster this limited future extensability so they are now detected in the file system and loaded to register themselves.

--- pydbsprocket/core/provider_discovery.py
+++ pydbsprocket/core/provider_discovery.py
@@ -1,8 +1,15 @@
-#Provider Mapping Dicts
 import importlib
 from typing import Callable
 
 db_provider_mapper={}
+
+'''
+Discovers Providers
+When a provider name is supplied go and see if a package with that
+name exists in data_providers namespace package. If it does load it into memory.
+The provider __init__.py is responsible for registering its callable with the db_provider_mapper dictionary.
+This is only ever called by db init which is protected by a lock so it is assumed no lock is required here
+'''
 
 def get_provider_callable(provider_name: str) -> Callable:
     if provider_name not in db_provider_mapper:
